207d5f281e0a432fbeb5a1d18c4165aa4440612e
KosenkoMarta <ingressjimenez@gmail.com>
2023-03-31 18:12:31 +0300

    fruit counting

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int apples = 2;
+        int bananas = 4;
+        int kiwi = 2;
+        int peaches = 5;
+
+        int s1 = apples + bananas;
+        int s2 = kiwi + peaches;
+
+        System.out.println(s2 > s1);
+
+        s1++;
+        s2 -= 2;
+
+        System.out.println(s1 > s2);
+
+        System.out.println(s1 % 2 == 0 ^ s2 % 2 == 0 || s1 % 2 == 0 && s2 % 2 == 0);
+    }
+}
